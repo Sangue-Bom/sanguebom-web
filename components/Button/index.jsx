2bbdec7edef9e759604be4c2380fb940569ec552
lscantelbury <lhsda.snf20@uea.edu.br>
2022-10-05 22:35:01 -0400

    Configuring tailwind classes

--- components/Button/index.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Button({children}) {
-	return(
-		<button className="w-full h-10 font-bold text-[#FFFFFF] rounded drop-shadow-xl bg-primary">
-			{children}
-		</button>
-	)
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
   theme: {
     colors: {
       'primary': '#FF5757',
-      'gray': 'F0F0F5'
+      'primary-dark': '#E82B5C',
+      'secondary': '#F7A3A6',
+      'gray-base': 'F0F0F5'
     }
   },
   plugins: [],
