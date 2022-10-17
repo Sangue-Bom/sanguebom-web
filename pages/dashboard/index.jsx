163374223566b6a7ccd5831884ca30006044f37a
lscantelbury <lhsda.snf20@uea.edu.br>
2022-10-17 19:23:18 -0400

    Rendering user name on dashboard

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,9 +1,13 @@
+import {useContext} from "react";
 import BasePage from "../../components/BasePage/index";
+import {LoginContext} from "../login/LoginContext";
 
 export default function Dashboard() {
+  const {user} = useContext(LoginContext);
+
   return (
     <BasePage>
-        <h1 className="mt-24 font-bold text-2xl text-gray-dark pb-5">Seja bem vindo, <span className="text-primary">Hospital de Teste</span>!</h1>
+      <h1 className="mt-24 font-bold text-2xl text-gray-dark pb-5">Seja bem vindo<span className="text-primary">{user.hosp_name ? `, ${user?.hosp_name}`: ""}</span>!</h1>
       <hr className="w-full h-[1px] border-none bg-[#C0C0C4] mb-14"/>
       <h1 className="font-bold text-2xl mg-6">Últimos informativos</h1>
     </BasePage>
