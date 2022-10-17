1365e91620379d3d28d8041d69022ed71b45ffd7
lscantelbury <lhsda.snf20@uea.edu.br>
2022-10-17 19:22:19 -0400

    Adding LoginProvider to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import SignUpProvider from "../pages/sign-up/SingUpContext";
+import {LoginProvider} from "./login/LoginContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SignUpProvider>
+      <LoginProvider>
       <Component {...pageProps} />
+      </LoginProvider>
     </SignUpProvider>
   );
 }
